5015bba5f93c4ee2484720623390682188ecb5f6
haiming <1072950174@qq.com>
2019-11-06 17:03:51 +0800

    增加了过度进度条

--- src/components/loadable.js
+++ src/components/loadable.js
@@ -1,6 +1,15 @@
 import React from 'react'
-// import propTypes from 'prop-types'
-import './common'
-const OverLoadTmpl = <div className='loading-process' />
-
-export default OverLoadTmpl
+import nprogress from 'nprogress'
+class OverLoadTmpl extends React.Component {
+  constructor (props) {
+    super(props)
+    nprogress.start()
+  }
+  componentWillUnmount () {
+    nprogress.done()
+  }
+  render () {
+    return null
+  }
+}
+export default <OverLoadTmpl />
