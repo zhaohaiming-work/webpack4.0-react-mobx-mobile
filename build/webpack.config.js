4b8b3672c2ba6e25b090720c8b4a8b547e26ff2f
zhaohaiming-work <1072950174@qq.com>
2019-12-17 15:15:48 +0800

    Update webpack.config.js
    去掉了生产环境打包happypack缓存

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -102,7 +102,7 @@ const babelLoader = {
 config.plugins.push(
   new HappyPack({
     id: 'happyBabel',
-    //cache: false,
+    cache: __DEV__,
     loaders: [babelLoader],
     //共享进程池
     threadPool: happyThreadPool,
