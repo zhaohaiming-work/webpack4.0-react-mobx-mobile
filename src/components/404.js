c42a3557a180d2c05a420bb00bceb8f564deda9f
haiming <1072950174@qq.com>
2020-04-16 17:20:42 +0800

    清空

--- src/components/404.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Result } from 'antd-mobile'
-const myImg = src => <img src={src} className='spe am-icon am-icon-md' alt='' />
-export default () => <Result
-  img={myImg('https://gw.alipayobjects.com/zos/rmsportal/GIyMDJnuqmcqPLpHCSkj.svg')}
-  title='404'
-  message='抱歉，页面没找到'
-/>
